9d7239a73ca268665141e345ea6d60307b24cd3a
ericward-noaa <eric.ward@noaa.gov>
2023-01-18 16:12:50 -0800

    docs

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -18,7 +18,7 @@ test_that("1D linear model works", {
   expect_equal("coefs", names(lm_example)[3])
 
   pred = predict(lm(response$dev ~ -1 + predictors[,1] + predictors[,2] + predictors[,3]))
-  expect_equal(lm_example$pred$est[40], pred[40][[1]])
+  expect_equal(lm_example$pred$est[40], pred[40][[1]], tolerance = 0.001)
 
 })
 
@@ -37,7 +37,7 @@ test_that("1D gam model works", {
                                     n_forecast = 1,
                                     n_years_ahead = 0,
                                     max_vars = 3)
-  expect_equal(gam_example$pred$est[40], -0.08842347)
+  expect_equal(gam_example$pred$est[40], -0.08842347, tolerance = 0.001)
 
   set.seed(123)
   gam_example <- univariate_forecast(response,
@@ -46,10 +46,10 @@ test_that("1D gam model works", {
                                      n_forecast = 4,
                                      n_years_ahead = 0,
                                      max_vars = 3)
-  expect_equal(gam_example$pred$est[37], -0.0003308037)
-  expect_equal(gam_example$pred$est[38], 0.2696979393)
-  expect_equal(gam_example$pred$est[39], -0.0094620398)
-  expect_equal(gam_example$pred$est[40], -0.0884234683)
+  expect_equal(gam_example$pred$est[37], -0.0003308037, tolerance = 0.001)
+  expect_equal(gam_example$pred$est[38], 0.2696979393, tolerance = 0.001)
+  expect_equal(gam_example$pred$est[39], -0.0094620398, tolerance = 0.001)
+  expect_equal(gam_example$pred$est[40], -0.0884234683, tolerance = 0.001)
 })
 
 
@@ -67,7 +67,7 @@ test_that("1D lasso model works", {
        model_type = "glmnet",
        n_forecast = 1,
        n_years_ahead = 1)
-  expect_equal(lm_example$pred$est[40], -0.3595902)
+  expect_equal(lm_example$pred$est[40], -0.3595902, tolerance = 0.001)
 
   set.seed(123)
   lm_example <- univariate_forecast_ml(response,
@@ -75,10 +75,10 @@ test_that("1D lasso model works", {
                                        model_type = "glmnet",
                                        n_forecast = 4,
                                        n_years_ahead = 1)
-  expect_equal(lm_example$pred$est[37], -0.2298422)
-  expect_equal(lm_example$pred$est[38], -0.3201412)
-  expect_equal(lm_example$pred$est[39], 1.3507670)
-  expect_equal(lm_example$pred$est[40], -0.3595902)
+  expect_equal(lm_example$pred$est[37], -0.2298422, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[38], -0.3201412, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[39], 1.3507670, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[40], -0.3595902, tolerance = 0.001)
 })
 
 
@@ -96,7 +96,7 @@ test_that("1D random forest model works", {
                                        model_type = "randomForest",
                                        n_forecast = 1,
                                        n_years_ahead = 1)
-  expect_equal(lm_example$pred$est[40], -0.02658667)
+  expect_equal(lm_example$pred$est[40], -0.02658667, tolerance = 0.001)
 
   set.seed(123)
   lm_example <- univariate_forecast_ml(response,
@@ -104,8 +104,8 @@ test_that("1D random forest model works", {
                                        model_type = "randomForest",
                                        n_forecast = 4,
                                        n_years_ahead = 1)
-  expect_equal(lm_example$pred$est[37], -0.2298422)
-  expect_equal(lm_example$pred$est[38], -0.3201412)
-  expect_equal(lm_example$pred$est[39], 1.3507670)
-  expect_equal(lm_example$pred$est[40], -0.3595902)
+  expect_equal(lm_example$pred$est[37], -0.07783784, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[38], 0.1210044, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[39], 0.4661449, tolerance = 0.001)
+  expect_equal(lm_example$pred$est[40], 0.1016875, tolerance = 0.001)
 })
